1ca1aa103684c89c65778add56b74087ec497af9
Alfredo Velasco <alfredo0234@gmail.com>
2023-10-26 02:51:29 -0500

    Added a player model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_24_005113) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_26_074959) do
   create_table "cards", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "rank"
     t.integer "color"
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
