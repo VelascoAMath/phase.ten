2c9c418210e8343eeb0d1af652c94a0a22610a53
Alfredo Velasco <avelasco@mcelroy.com>
2023-12-29 14:09:21 -0600

    Don't add unique player username index if it exists

--- db/migrate/20231125202944_add_display_name_to_player.rb
+++ db/migrate/20231125202944_add_display_name_to_player.rb
@@ -2,7 +2,7 @@ class AddDisplayNameToPlayer < ActiveRecord::Migration[7.1]
   def change
     add_column :players, :display_name, :string, null: false
     rename_column :players, :name, :user_name
-    change_column_null :players, :user_name, null: false
-    add_index :players, :user_name, unique: true
+    change_column_null :players, :user_name, false
+    add_index :players, :user_name, unique: true, if_not_exists: true
   end
 end
