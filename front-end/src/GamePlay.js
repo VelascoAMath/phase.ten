78c3d9c2bef31c17ec3f561a1ef9f7aed3b3754f
Alfredo Velasco <avelasco@mcelroy.com>
2023-12-29 14:11:39 -0600

    Added a new page where the player will actually play. For now, we're just testing

---
+++ front-end/src/GamePlay.js
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from "react";
+import useFetch from "./useFetch";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import callPOST from "./callPOST";
+
+
+export default function GamePlay() {
+    const socket = new WebSocket("ws://localhost:8001");
+    const [sockMessage, setSockMessage] = useState([]);
+
+    socket.onopen = (event) => {
+      send("Hello!")
+    }
+
+    socket.onmessage = (event) => {
+        console.log("on message is being called");
+        console.log(event);
+        setSockMessage([...sockMessage, event.data]);
+    }
+
+    const send = function(message){
+        if (socket.readyState == socket.OPEN){
+            socket.send(JSON.stringify(message));
+        }
+    }
+
+    const getClassFromRank = function(rank) {
+        if(rank === 'S'){
+            return 'card S';
+        }
+        if(rank === 'W'){
+            return 'card W';
+        }
+        return 'card';
+    }
+
+    const createDeck = function() {
+        const colorOrder = ["red", "blue", "green", "yellow", "S", "S", "W", "W"];
+        const rankOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "S", "W", "W"];
+
+        let result = [];
+
+        let id = 0;
+        for(const color of colorOrder){
+            for(const rank of rankOrder){
+                    for(let i = 0; i < 2; i++){
+                    if((color === "W" || rank === "W") && color !== rank){
+                        continue;
+                    }
+                    if((color === "S" || rank === "S") && color !== rank){
+                        continue;
+                    }
+                    result.push({"rank": rank, "color": color === "S" ? "black" : color, "id": id++});    
+                }
+            }
+        }
+
+        result.sort((a, b) => {return 2 * Math.random() - 1} )
+
+        return result;
+    }
+
+    const initialDeck = createDeck();
+    const initialHand = [];
+    for(const i of Array(10)){
+        initialHand.push(initialDeck.pop());
+    }
+    const [discard, setDiscard] = useState([initialDeck.pop()]);
+    const [deck, setDeck] = useState(initialDeck);
+    const [hand, setHand] = useState(initialHand);
+    const [playerState, setPlayerState] = useState("Drawing");
+    const [cardToDiscard, setCardToDiscard] = useState(-1);
+    console.log(hand);
+    console.log(deck);
+
+
+    const cardToDiv = function(card, onClick) {
+        return <div style={{backgroundColor: card.color}} className={getClassFromRank(card.rank)} key={card.id} onClick={onClick}>{card.rank}</div>;
+    }
+
+    const getDeckDivs = function (deck, onClick) {
+        return deck.map((x) => cardToDiv(x, onClick));
+    }
+
+    
+    return (
+        <div style={{display: "flex", flexDirection: "column", rowGap: "20px"}}>
+            <div className="player-console">
+                {getDeckDivs(discard)}
+                <div className="card" style={{backgroundColor:"#004998"}}>Phase 10</div>
+            </div>
+            <div className="player-console">
+                {playerState == "Drawing" && <Button onClick={ () => {hand.push(deck.pop()); setHand(hand); setDeck(deck); setPlayerState("Discarding")} }>Draw from the Deck</Button>}
+                {playerState == "Drawing" && <Button onClick={ () => {hand.push(discard.pop()); setHand(hand); setDiscard(discard); setDeck(deck); setPlayerState("Discarding") } }>Draw from the Discard</Button>}
+                {playerState == "Discarding" && <Button onClick={ () => {if(cardToDiscard == -1){return;} send("Complete Turn"); setPlayerState("Drawing"); setDiscard([...discard, hand.filter((x)=> x.id == cardToDiscard)[0] ]); setHand(hand.filter((x) => x.id != cardToDiscard)) } }>Send Message</Button>}
+            </div>
+            <div className="card-collection">
+                {hand.map((x, idx) => <div style={{backgroundColor: x.color}} className={getClassFromRank(x.rank)} key={x.id} onClick={() => {setCardToDiscard(x.id)}}>{x.rank}</div>)}
+                Discarding: {cardToDiscard != -1 && JSON.stringify(hand.filter(x => x.id ==cardToDiscard)[0])} {cardToDiscard}
+            </div>
+            <div style={{width: "50%", overflow: "scroll", height: "50vh"}}>
+                <h2>Received message</h2>
+                {sockMessage.map(x => <div>{x}</div>)}
+            </div>
+        </div>
+    )
+}
