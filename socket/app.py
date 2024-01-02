88a9e776d6540800d1bb36ea00cf8f0c9a3ad071
Alfredo Velasco <avelasco@mcelroy.com>
2024-01-02 14:17:53 -0600

    Added some broadcasting

--- socket/app.py
+++ socket/app.py
@@ -4,17 +4,27 @@ import asyncio
 import websockets
 import json
 
+connected = set()
+socket_index = 0
 
 async def handler(websocket):
-    while True:
-        message = await websocket.recv()
-        message = json.loads(message)
-        print(f"Received {message}")
-        if message == "Hello!":
-            continue
-        await websocket.send(json.dumps(message))
-        print(f"Sending {message}")
-
+    global socket_index
+    index = socket_index
+    socket_index += 1
+    connected.add(websocket)
+    try:
+        while True:
+            message = await websocket.recv()
+            message = json.loads(message)
+            print(f"Received {message}")
+            if message == "Hello!":
+                continue
+            message = message + f" {index}"
+            websockets.broadcast(connected, json.dumps(message))
+            # await websocket.send(json.dumps(message))
+            print(f"Sending {message}")
+    finally:
+     connected.remove(websocket)
 
 
 async def main():
