2ecb65768d238ec5a915c0bb82e16c69166161f7
Alfredo Velasco <avelasco@mcelroy.com>
2024-06-03 16:07:03 -0500

    Renamed 'inputs' to 'state'

--- front-end/src/InputReducer.js
+++ front-end/src/InputReducer.js
@@ -1,10 +1,10 @@
 
-export default function inputReducer(inputs, action) {
+export default function inputReducer(state, action) {
 
     switch(action.type) {
         case 'change-input': {
             localStorage.setItem(action.key, action.value);
-            return {...inputs, [action.key]: action.value};
+            return {...state, [action.key]: action.value};
         }
         case 'set-state': {
             for(const [key, val] of Object.entries(action.nextState)){
@@ -14,12 +14,12 @@ export default function inputReducer(inputs, action) {
         }
         case '2': {
             alert('2');
-            return inputs;
+            return state;
         }
         default: {
-            console.log(inputs);
+            console.log(state);
             console.log(action);
-            return inputs;
+            return state;
         }
     }
 }
