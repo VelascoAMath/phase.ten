8f5018251608749483fca3256d85bbaff16bf662
Alfredo Velasco <avelasco@mcelroy.com>
2024-06-05 15:15:29 -0500

    Improved the look of the game room

--- front-end/src/GameRoom.js
+++ front-end/src/GameRoom.js
@@ -2,11 +2,16 @@ import React from "react";
 import Button from 'react-bootstrap/Button';
 
 
-const joinGameButton = function(game, title, key, onClick){
+
+
+const joinGameButton = function(game, title, key, onClick, user_id){
     const nameList = game.users.map(user => {return <div key={user.id}>{user.name}</div>});
+    // If the user is already in the game, we cannot join
+    // This will mark the CSS and also disable clicking events
+    const isJoinable = game.users.filter(user => {return user.id === user_id}).length === 0;
     
     return (
-        <div style={{backgroundColor: "brown", cursor: "pointer"}} key={key} onClick={onClick}>
+        <div className={"room-to-join " + ( isJoinable ? "joinable":"non-joinable")} key={key} onClick={isJoinable ? onClick: null}>
             {title}
             {nameList}
         </div>
@@ -45,7 +50,9 @@ export default function PlayerPage({props}) {
                 Join one of the games below
             </div>
             {(!state["game-list"] || state["game-list"].length === 0) && "There seem to be no games. Why don't you create one?"}
-            {state["game-list"] && state["game-list"].map((x, idx) => joinGameButton(x, `Game ${idx+1}` ,x.id, () => {joinGame(x.id)}))}
+            <div className="rooms-to-join">
+                {state["game-list"] && state["game-list"].map((x, idx) => joinGameButton(x, `Game ${idx+1}` ,x.id, () => {joinGame(x.id)}, state["user-id"] ))}
+            </div>
         </div>
     )
 }
