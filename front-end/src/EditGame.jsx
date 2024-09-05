c70a16031ca487cf34e83f45010d7ac28c135813
Alfredo Velasco <avelasco@mcelroy.com>
2024-09-05 15:22:14 -0500

    Hosts can now modify the phases of their games

--- front-end/src/EditGame.jsx
+++ front-end/src/EditGame.jsx
@@ -1,27 +1,39 @@
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "wouter";
-
-
-
 
 
 export default function EditGame({props}) {
     const{state, dispatch, socket} = props;
-	const params = useParams();
+    const params = useParams();
     let [, navigate] = useLocation();
-	const game_id = params?.id;
+    const game_id = params?.id;
     const user_id = state["user-id"];
     const game = state["game-list"].filter((g) => g.id == game_id)[0];
+
+    const [phaseInput, setPhaseInput] = useState(game?.phase_list);
+
+    useEffect(() => {
+        setPhaseInput(game?.phase_list);
+    }, [game])
 
     if(!game){
         return <div>{game_id} is not a valid game id!</div>
     }
 
-
     return (
         <div>
             <h1>Edit game {game.id}</h1>
             <h2>Phase List</h2>
-            {JSON.stringify(game.phase_list)}
+            {phaseInput?.map((phase, idx) => {
+                return <input value={phase} onChange={(e) => {let phaseInputCopy = [...phaseInput]; phaseInputCopy[idx] = e.target.value.toLocaleUpperCase(); setPhaseInput(phaseInputCopy)}}/>
+            })}
+            <div>
+                <button onClick={() => {setPhaseInput([...phaseInput, ""])}} >Add Phase</button>
+                <button onClick={() => {setPhaseInput(phaseInput.filter((_, idx) => {return idx < phaseInput.length - 1}))}} >Remove Phase</button>
+                <button onClick={() => {if(socket.readyState === socket.OPEN){socket.send(JSON.stringify({type: "edit_game_phase", "user_id": user_id, "game_id": game_id, new_phase: phaseInput }))} }}>Change Phase</button>
+                <button onClick={() => {if(socket.readyState === socket.OPEN){socket.send(JSON.stringify({type: "edit_game_phase", "user_id": user_id, "game_id": game_id, new_phase: ["S3+S3","S3+R4","S4+R4","R7","R8","R9","S4+S4","C7","S5+S2","S5+S3"] }))} }} >Reset</button>
+            </div>
+
             <h2>Game Type</h2>
             {game.type}
             <h2>Players</h2>
