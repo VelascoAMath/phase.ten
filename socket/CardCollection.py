a643b8af4577c9cfccc12bbe160698e9433af10e
Alfredo Velasco <avelasco@mcelroy.com>
2024-11-22 11:05:09 -0600

    Added new string and equality magic methods

--- socket/CardCollection.py
+++ socket/CardCollection.py
@@ -4,18 +4,34 @@ from Card import Card
 
 
 class CardCollection(list):
-
+    
+    def __eq__(self, other):
+        if isinstance(other, CardCollection):
+            if len(self) != len(other):
+                return False
+            
+            for i in range(len(self)):
+                if self[i] != other[i]:
+                    return False
+            return True
+            
+        else:
+            raise Exception(f"Cannot compare CardCollection with item of type {type(other)}!")
+    
+    def __str__(self):
+        return f"{[str(c) for c in self]}"
+    
     def to_json(self):
         return json.dumps(self.to_json_dict())
-
+    
     def to_json_dict(self):
         return [x.to_json_dict() for x in self]
-
+    
     @staticmethod
     def from_json(data):
         data = json.loads(data)
         return CardCollection.from_json_dict(data)
-
+    
     @staticmethod
     def from_json_dict(data):
         return CardCollection([Card.fromJSONDict(card) for card in data])
@@ -23,7 +39,7 @@ class CardCollection(list):
 
 def main():
     deck = CardCollection(Card.getNewDeck())
-
+    
     print(deck)
     print(deck == CardCollection.from_json(deck.to_json()))
 
